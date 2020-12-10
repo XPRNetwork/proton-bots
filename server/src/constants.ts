d6e0088df9d9ae8205d6fad66418ae9152a2b656
Metallicus Dev <>
2020-12-10 01:17:31 -0400

    test oracle

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -16,14 +16,15 @@ if (!process.env.PRIVATE_KEY) {
 
 export const ORACLE_CONTRACT = "oracles"
 export const ORACLES = [
-    { oracle_index: 0, baseCurrency: 'XPR', baseId: 'proton', quoteCurrency: 'BTC', quoteId: 'btc' }
+    { oracle_index: 0, baseCurrency: 'XPR', baseId: 'proton', quoteCurrency: 'BTC', quoteId: 'btc' },
+    { oracle_index: 1, baseCurrency: 'BTC', baseId: 'bitcoin', quoteCurrency: 'USD', quoteId: 'usd' }
 ]
 
 export const BOTS_CONTRACT = "bot"
 export const BOTS_ACCOUNTS: BotAccount[] = [
-    { bot_index: 0, name: 'bot1', permission: 'active', parallel: Math.ceil(500 / 86400) * 80, timer: (86400 / 500) * 1000, ...ORACLES[0] },
-    { bot_index: 1, name: 'bot2', permission: 'active', parallel: Math.ceil(5000 / 86400) * 80, timer: (86400 / 5000) * 1000, ...ORACLES[0] },
-    { bot_index: 2, name: 'bot3', permission: 'active', parallel: Math.ceil(50000 / 86400) * 80, timer: (86400 / 50000) * 1000, ...ORACLES[0] },
-    { bot_index: 3, name: 'bot4', permission: 'active', parallel: Math.ceil(500000 / 86400) * 80, timer: (86400 / 500000) * 1000 , ...ORACLES[0] }
+    { bot_index: 0, name: 'bot1', permission: 'active', parallel: Math.ceil(500 / 86400) * 80, timer: (86400 / 500) * 1000, ...ORACLES[1] },
+    { bot_index: 1, name: 'bot2', permission: 'active', parallel: Math.ceil(5000 / 86400) * 80, timer: (86400 / 5000) * 1000, ...ORACLES[1] },
+    { bot_index: 2, name: 'bot3', permission: 'active', parallel: Math.ceil(50000 / 86400) * 80, timer: (86400 / 50000) * 1000, ...ORACLES[1] },
+    { bot_index: 3, name: 'bot4', permission: 'active', parallel: Math.ceil(500000 / 86400) * 80, timer: (86400 / 500000) * 1000 , ...ORACLES[1] }
 ]
 export const PRICE_FETCH_TIMER = 5000

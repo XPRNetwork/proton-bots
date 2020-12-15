1aabc02071d21b57623c3b16830959de6f091fdd
Metallicus Dev <>
2020-12-14 21:58:22 -0400

    use ACTIONS_MULTIPLIER

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ const process = async (account: BotAccount, index: number = 1) => {
     const price = prices[account.baseId][account.quoteId]
 
     // @ts-ignore-next-line
-    const actions = Array(i).fill().map((_) => ({
+    const actions = Array(ACTIONS_MULTIPLIER).fill().map((_) => ({
         account: BOTS_CONTRACT,
         name: 'process',
         data: {
